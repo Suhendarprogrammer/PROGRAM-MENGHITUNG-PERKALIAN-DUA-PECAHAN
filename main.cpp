d488c9133968833573418b422cb64498cf1a4cd0
Suhendar <45907948+Suhendarprogrammer@users.noreply.github.com>
2018-12-31 00:04:51 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int p1,p2,hasil;
+    cout<<"********PROGRAM MENGHITUNG PERKALIAN DUA PECAHAN********\n";
+    cout<<"========================================================\n";
+    cout<<"\nMasukkan Nilai P1  : ";
+    cin>>p1;
+    cout<<"\nMasukkan Nilai P2  : ";
+    cin>>p2;
+    cout<<"\n=======HASILNYA======\n";
+    p1=3/4;
+    p2=2/3;
+    hasil=(3*2)/(4*3);
+    cout<<"\nMaka Hasil Dari Perkalian Dua Tersebut Adalah  : "<<hasil<<"\n";
+    return 0;
+}
